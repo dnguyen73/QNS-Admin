31188559d643fb1a7fb624aa642a5ae421b5d89e
Duy Nguyen <dnguyen73@csc.com>
2017-11-20 00:16:20 +0700

    change prod env endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,8 @@ export const environment = {
   production: true,
 
   //URL of production API (to be updated)
-  apiUrl: 'http://localhost:3000/api'
+  apiUrl: 'http://139.59.102.174/:3000/api',
+
+  //Domain address
+  FILE_HOST_URL: 'http://139.59.102.174/:3000/files'
 };
